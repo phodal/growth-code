56050d2805e037825dd714c859fbff883d172cea
Fengda HUANG <h@phodal.com>
2016-11-04 07:41:06 +0800

    [ch5] add basic hello world demo

--- chapter5/growth_studio/settings.py
+++ chapter5/growth_studio/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'growth_studio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- chapter5/growth_studio/urls.py
+++ chapter5/growth_studio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from homepage.views import index as home
 
 urlpatterns = [
+    url(r'^$', home, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
