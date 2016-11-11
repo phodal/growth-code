ae9ba3c6bfdb5f9e47a16e8d8f8585d73fd2edce
Fengda HUANG <h@phodal.com>
2016-11-11 16:29:47 +0800

    [ch5] format code

--- chapter5/blog/tests.py
+++ chapter5/blog/tests.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from blog.models import Blog
 from blog.views import blog_detail
 
+
 class BlogpostTest(TestCase):
     def test_blogpost_url_resolves_to_blog_post_view(self):
         found = resolve('/blog/this_is_a_test.html')
@@ -12,6 +13,6 @@ class BlogpostTest(TestCase):
 
     def test_blogpost_create_with_view(self):
         Blog.objects.create(title='hello', author='admin', slug='this_is_a_test', body='This is a blog',
-                                posted=datetime.now)
+                            posted=datetime.now)
         response = self.client.get('/blog/this_is_a_test.html')
         self.assertIn(b'This is a blog', response.content)
