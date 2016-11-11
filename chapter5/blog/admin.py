61601e8858e2305a62910a02e3b8c5edd92a246a
Fengda HUANG <h@phodal.com>
2016-11-11 22:27:46 +0800

    [ch5] simplify admin code

--- chapter5/blog/admin.py
+++ chapter5/blog/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from blog.models import Blog
 
-
-class BlogAdmin(admin.ModelAdmin):
-    exclude = ['posted']
-    prepopulated_fields = {'slug': ('title',)}
-
-
-admin.site.register(Blog, BlogAdmin)
+admin.site.register(Blog)
