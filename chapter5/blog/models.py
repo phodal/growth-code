fff0bca3ee51279bda1d56cd857930965fe798fe
Fengda HUANG <h@phodal.com>
2016-11-11 16:22:09 +0800

    [ch5] add tests

--- chapter5/blog/models.py
+++ chapter5/blog/models.py
@@ -1,4 +1,3 @@
-from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models import permalink
 
--- chapter5/blog/tests.py
+++ chapter5/blog/tests.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
-# Create your tests here.
+from blog.models import Blog
+from blog.views import blog_detail
+
+class BlogpostTest(TestCase):
+    def test_blogpost_url_resolves_to_blog_post_view(self):
+        found = resolve('/blog/this_is_a_test.html')
+        self.assertEqual(found.func, blog_detail)
+
+    def test_blogpost_create_with_view(self):
+        Blog.objects.create(title='hello', author='admin', slug='this_is_a_test', body='This is a blog',
+                                posted=datetime.now)
+        response = self.client.get('/blog/this_is_a_test.html')
+        self.assertIn(b'This is a blog', response.content)
