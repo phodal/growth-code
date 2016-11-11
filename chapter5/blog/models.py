63b105ddf8a100a759ac55c00514ed89e755c795
Fengda HUANG <h@phodal.com>
2016-11-11 16:46:14 +0800

    [ch5] thinking in i18n

--- chapter5/blog/models.py
+++ chapter5/blog/models.py
@@ -1,16 +1,22 @@
+# coding=utf-8
 from django.db import models
 from django.db.models import permalink
+from django.utils.translation import ugettext_lazy as _
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    author = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    body = models.TextField()
+    class Meta:
+        verbose_name = _('博客')
+        verbose_name_plural = _('博客')
+
+    title = models.CharField(max_length=100, unique=True, verbose_name=_('标题'))
+    author = models.CharField(max_length=100, unique=True, verbose_name=_('作者'))
+    slug = models.SlugField(max_length=100, unique=True, verbose_name=_('URL'))
+    body = models.TextField(verbose_name=_('正文'))
     posted = models.DateField(db_index=True, auto_now_add=True)
 
     def __str__(self):
-        return '%s - %s' % (self.posted, self.title)
+        return '%s' % (self.title)
 
     @permalink
     def get_absolute_url(self):
--- chapter5/growth_studio/settings.py
+++ chapter5/growth_studio/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'UTC'
 
