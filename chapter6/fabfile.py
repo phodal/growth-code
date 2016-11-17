f6ff314dd3f7285a339a27287ff8ee3d0a8ac6e2
Fengda HUANG <h@phodal.com>
2016-11-18 07:34:49 +0800

    [ch6] add basic deploy scripts

--- chapter6/fabfile.py
+++ chapter6/fabfile.py
@@ -1,6 +1,7 @@
 from fabric.api import local
 from fabric.decorators import task
 from fabric.context_managers import settings, hide
+from fabric.operations import sudo, run
 
 
 @task
@@ -21,17 +22,18 @@ def pep8():
     with settings(hide('warnings'), warn_only=True):
         local('pep8 .')
 
+
 @task
 def tag_version(version):
     """Tag New Version"""
     local("git tag %s" % version)
-    local("git push origin %s"%version)
+    local("git push origin %s" % version)
 
 
 @task
 def fetch_version(version):
     """Fetch Git Version"""
-    local('wget https://codeload.github.com/phodal/growth_studio/tar.gz/%s'%version)
+    local('wget https://codeload.github.com/phodal/growth_studio/tar.gz/%s' % version)
 
 
 @task
@@ -39,3 +41,36 @@ def test():
     """ Run Test """
     local("./manage.py test")
 
+
+@task
+def setup():
+    """ Setup the Ubuntu Env """
+    sudo('apt-get update')
+    APT_GET_PACKAGES = [
+        "build-essential",
+        "git",
+        "python3-dev",
+        "python3-virtualenv",
+        "python3-pip",
+        "nginx",
+        "virtualenv",
+    ]
+    sudo("apt-get install " + " ".join(APT_GET_PACKAGES))
+    sudo('pip3 install virtualenv')
+    run('virtualenv --distribute -p /usr/bin/python3.5 py35env')
+
+
+@task
+def deploy(version):
+    """ depoly app to cloud """
+    run('cd ~/')
+    run(('wget ' + 'https://codeload.github.com/phodal/growth_studio/tar.gz/v' + '%s') % version)
+    run('tar xvf v%s' % version)
+
+    # run('rm -rf growth-studio')
+    # run('mv growth-studio-%s growth-studio'%version)
+    # run('rm v%s'%version)
+
+    run('source py35env/bin/activate')
+    run('pip3 install -r growth-studio-%s/requirements/prod.txt' % version)
+    run('ln -s growth-studio-%s growth-studio' % version)
