bfb014b65932902f9cc07bc85c3aadfdb320c438
Fengda HUANG <h@phodal.com>
2016-11-17 17:33:17 +0800

    [ch5] add static config

--- chapter5/growth_studio/settings.py
+++ chapter5/growth_studio/settings.py
@@ -124,4 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
