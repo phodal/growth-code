a0916ae72714f8a26c3df8e247d4036a2d5cadd7
Fengda HUANG <h@phodal.com>
2016-11-27 19:03:28 +0800

    [ch7] add cache demo

--- chapter7/blog/views.py
+++ chapter7/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.views.decorators.cache import cache_page
 
 from blog.models import Blog
 
 
+@cache_page(60 * 15)
 def blog_list(request):
     return render_to_response('blog/list.html', {
         'blogs': Blog.objects.all()
