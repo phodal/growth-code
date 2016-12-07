1216640b5374a0408a31860725983857a18492dd
Fengda HUANG <h@phodal.com>
2016-12-07 14:43:54 +0800

    [ch9] simplify code

--- chapter9/blog/api.py
+++ chapter9/blog/api.py
@@ -1,21 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from rest_framework import serializers, viewsets
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import BasePermission
 
 from blog.models import Blog
 
 SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
-
-
-class IsAuthenticatedOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        if (request.method in SAFE_METHODS or
-                    request.user and
-                    request.user.is_authenticated()):
-            return True
-        return False
 
 
 class BlogSerializer(serializers.ModelSerializer):
